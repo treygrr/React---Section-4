52385d447f5f3573be0897cde9a5fbefcc98cb7d
Trey <trey@lavidadev.com>
2019-06-13 15:56:57 -0400

    Lesson 52, 53: Add constructor and state object to pass in latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    // Not required by react
+    // Constructor is called first in a class. Destructor is called last.
+    constructor(props) {
+        super(props);
+
+        // Initialize state object
+        this.state = {
+            lat: null
+        };
+    }
+
+    // React says that we have to define render!!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
