ee42e2b43a5dc1dad53a40b57657a6765c849a36
Trey <trey@lavidadev.com>
2019-06-13 16:07:08 -0400

    Lesson 54: Updating State Properties

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,22 +9,32 @@ class App extends React.Component {
 
         // Initialize state object
         this.state = {
-            lat: null
+            lat: null,
+            long: null
         };
-    }
-
-    // React says that we have to define render!!
-    render() {
+        // We don't usually want async or code that takes time to execute in the render function.
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                console.log(position)
+                // We use setState only to set the state. Do not use other traditional ways to try and set it.
+                // This will simply not work.
+                this.setState({ lat: position.coords.latitude });
+                this.setState({ long: position.coords.longitude });
             },
             (err) => {
                 console.log(err)
             } 
         );
+    }
 
-        return <div>Latitude: </div>
+
+    // React says that we have to define render!!
+    render() {
+        return (
+            <div className="InfoWrapper">
+                <div>Latitude: {this.state.lat}</div>
+                <div>Long: {this.state.long}</div>
+            </div>
+        );
     }
 }
 
