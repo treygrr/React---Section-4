7e1c9b9859f33145c0b35a2dabcda86d9b584293
Trey <trey@lavidadev.com>
2019-06-14 14:53:12 -0400

    Lesson 65: Showing Icons

--- seasons/src/seasonDisplay.js
+++ seasons/src/seasonDisplay.js
@@ -11,11 +11,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
     const text = season === 'winter' ? 'Burrr, it is chilly!' : 'Lets hit the beach!'
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
     return (
         <div className="LocationWrapper">        
             <div>Latitude: {props.lat}</div>
             <div>Longitude: {props.long}</div>
+            <i className={`${icon} icon`}></i>
             <h1>{ text }</h1>
+            <i className={`${icon} icon`}></i>
         </div>
     );
 }
