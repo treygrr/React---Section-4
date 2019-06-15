219ba302899f8de8add814c77909cd0bfde5bfda
Trey <trey@lavidadev.com>
2019-06-15 15:50:44 -0400

    Lesson 83: Handling Form Submittal

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -15,10 +15,16 @@ class SearchBar extends React.Component  {
         // console.log('input was clicked');
     }
 
+    onFormSubmit (event) {
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" 
