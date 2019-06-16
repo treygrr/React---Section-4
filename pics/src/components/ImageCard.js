2bcf5df7e49f16e5d6bf0436062848f24e4a7b76
Trey <trey@lavidadev.com>
2019-06-16 15:41:48 -0400

    Lesson 106: Dynamic Spans
    Turns out my solution was really close to what the instructor was going
    to teach.

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -3,30 +3,30 @@ import React from 'react';
 class ImageCard extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = { spans: 0 }
         this.imageref = React.createRef();
     }
 
     componentDidMount = () => {
-        console.log(this.calcSpan(this.getImageHeight()));
-        const spanning = this.calcSpan();
+        this.imageref.current.addEventListener('load', this.calcSpan);
     }
 
     calcSpan = () => {
-        let height = this.imageref.current.clientHeight;
-        let calc = Math.ceil(height / 200);
-        return calc;
+        const height = this.imageref.current.clientHeight;
+        console.log(height);
+        const calcs = Math.ceil(height / 10);
+        console.log(calcs);
+        this.setState({ spans: calcs });
     }
 
     render() {
         const { alt_description, urls }  = this.props.image;
         return(
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
                 <img 
                     alt={alt_description}
                     src={urls.regular}
                     ref={this.imageref}
-                    style={{gridRow: `${this.spanning}`}}
                 />
             </div>
         );
