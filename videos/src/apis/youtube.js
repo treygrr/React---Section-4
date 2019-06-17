814f3b7dcd0203aa3954f7f5eb3154e9759224a1
Trey <trey@lavidadev.com>
2019-06-17 13:42:49 -0400

    Add axios default export

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCbq5Ppo2m86ozXkXCgHlLeZot9S8yxO44';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
+
