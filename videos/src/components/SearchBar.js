a78e6e7e10a125684cf82c43bae42588e741f626
Trey <trey@lavidadev.com>
2019-06-17 13:19:00 -0400

    Lesson 112: Handling Form Submittal

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: 'Enter Search Terms' }
+    }
+
+    onInputChange = (event) => {
+        this.setState({ term : event.target.value});
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO: Make sure we call -- callbacck from parent component
+    }
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form 
+                className="ui form"
+                onSubmit={this.onFormSubmit}>
                     <label>Video Search</label>
-                    <input type="text" className=""></input>
+                    <input 
+                        type="text" 
+                        value={this.state.term} 
+                        className=""
+                        onChange={this.onInputChange}
+                        ></input>
                 </form>
             </div>
         );
