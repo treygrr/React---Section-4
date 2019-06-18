b02ab9f62130deec95c3ea4b08e56fef60c3c4d9
Trey <trey@lavidadev.com>
2019-06-18 01:14:06 -0400

    Lesson 127: Fixing a Few Warnings and adding a grid UI from Semantic UI

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -6,12 +6,12 @@ const VideoItem = ({video, onVideoSelect}) => {
         <div
             onClick={() => onVideoSelect(video)}
             className="item video-item"
-            >
+        >
             <img 
                 src={video.snippet.thumbnails.medium.url} 
                 alt={video.snippet.title}
                 className="ui image"
-                />
+            />
                 <div className="content">
                     <div className="header">
                         {video.snippet.title}
