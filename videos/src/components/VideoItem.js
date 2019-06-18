4b5272fb06ee6a964f3b9926b2e39b14e88e40bf
Trey <trey@lavidadev.com>
2019-06-18 02:25:57 -0400

    Add various styling fixes

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -5,18 +5,18 @@ const VideoItem = ({video, onVideoSelect}) => {
     return(
         <div
             onClick={() => onVideoSelect(video)}
-            className="item video-item"
+            className="item video-item ui segment"
         >
             <img 
                 src={video.snippet.thumbnails.medium.url} 
                 alt={video.snippet.title}
                 className="ui image"
             />
-                <div className="content">
-                    <div className="header">
-                        {video.snippet.title}
-                    </div>
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
                 </div>
+            </div>
             
         </div>
     );
