dc63dea45213fc2b0e5e4ee58b0b9c93a7fb0aa0
Trey <trey@lavidadev.com>
2019-06-18 02:25:01 -0400

    Add default search result

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -13,6 +13,10 @@ class App extends React.Component {
          };
     }
 
+    componentDidMount = () => {
+        this.onTermSubmit('Shane and Jeffree Star');
+    }
+
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
@@ -20,7 +24,10 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items});
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     }
 
     onVideoSelect = (video) => {
