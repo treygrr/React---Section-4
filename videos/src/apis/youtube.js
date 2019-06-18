c6363d524b96b3751cd5577a26479f5e5bba1730
Trey <trey@lavidadev.com>
2019-06-18 02:24:48 -0400

    Add fix for search to only show video results

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -4,7 +4,8 @@ const KEY = 'AIzaSyCbq5Ppo2m86ozXkXCgHlLeZot9S8yxO44';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
+    params: { 
+        type: 'video',
         part: 'snippet',
         maxResults: 5,
         key: KEY,
