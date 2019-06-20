e7880547ae8ace1521c44ad0d935ea03fef56b3e
Trey <trey@lavidadev.com>
2019-06-19 23:57:41 -0400

    Lesson 148: Configuring Connect with MapStateToProps

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 class SongList extends Component {
     render() {
         return (
@@ -8,4 +9,8 @@ class SongList extends Component {
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+}
+
+export default connect(mapStateToProps)(SongList);
